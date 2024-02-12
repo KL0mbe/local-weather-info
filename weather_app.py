3f35cea4a69ddaebcf1acca66bb7a5581d38fd91
Metin Özkaya <klombe@outlook.dk>
2024-02-12 21:50:12 +0100

    separated the temperatures into lists of their days

--- weather_app.py
+++ weather_app.py
@@ -1,6 +1,6 @@
 import requests
 import os
-import datetime
+from datetime import datetime, timedelta
 # city_name = input("please type your city name: ")
 # country_code = input("please type your country code: ")
 city_name = "ikast"
@@ -14,14 +14,34 @@ forecast_weather = requests.get(
     f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}&units=metric")
 curr_dict = curr_weather.json()
 forecast_dict = forecast_weather.json()
-"""need to use datetime module to remove the time from dt_txt(done)
-perhaps store date and temp as key value pair in a dict?"""
-for key in forecast_dict["list"]:
-    dt = datetime.datetime.fromisoformat(key["dt_txt"])
-    print(dt.date())
-    # print(key["dt_txt"])
-    # print(key["main"]["temp"])
 
+"functioning temp separator by day; need to attempt it in a for loop to cut down on the if elif length"
+forecast_temps = []
+for key in forecast_dict["list"]:
+    dt = datetime.fromisoformat(key["dt_txt"])
+    today = datetime.today().date()
+    if dt.date() == today + timedelta(days=1):
+        if len(forecast_temps) < 1:
+            forecast_temps.append([])
+        forecast_temps[0].append(key["main"]["temp"])
+    elif dt.date() == today + timedelta(days=2):
+        if len(forecast_temps) < 2:
+            forecast_temps.append([])
+        forecast_temps[1].append(key["main"]["temp"])
+    elif dt.date() == today + timedelta(days=3):
+        if len(forecast_temps) < 3:
+            forecast_temps.append([])
+        forecast_temps[2].append(key["main"]["temp"])
+    elif dt.date() == today + timedelta(days=4):
+        if len(forecast_temps) < 4:
+            forecast_temps.append([])
+        forecast_temps[3].append(key["main"]["temp"])
+    elif dt.date() == today + timedelta(days=5):
+        if len(forecast_temps) < 5:
+            forecast_temps.append([])
+        forecast_temps[4].append(key["main"]["temp"])
+
+print(forecast_temps)
 # print(f"the current weather in {curr_dict['name']} is {curr_dict['main']['temp']}ºC")
 
 
