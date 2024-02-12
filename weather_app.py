9620fc62bd6dd224874d2ed11535b7b8365652c9
Metin Özkaya <klombe@outlook.dk>
2024-02-12 20:08:43 +0100

    pulled forecasted dates from the forecast api

--- weather_app.py
+++ weather_app.py
@@ -1,18 +1,26 @@
 import requests
 import os
-city_name = input("please type your city name: ")
-country_code = input("please type your country code: ")
-# city_name = "ikast"
-# country_code = "dk"
+# city_name = input("please type your city name: ")
+# country_code = input("please type your country code: ")
+city_name = "ikast"
+country_code = "dk"
 api_key = os.environ.get("OPEN_WEATHER_API_KEY")
-r_location = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_code}&appid={api_key}")
-# &limit={limit}
-# r_forecast = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}&units=metric")
-r_location_dict = r_location.json()[0]
-lat, lon = r_location_dict["lat"], r_location_dict["lon"]
-r_curr_weather = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric")
-r_curr_dict = r_curr_weather.json()
+location = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_code}&appid={api_key}")
+lat, lon = location.json()[0]["lat"], location.json()[0]["lon"]
+curr_weather = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric")
+forecast_weather = requests.get(
+    f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}&units=metric")
+curr_dict = curr_weather.json()
+forecast_dict = forecast_weather.json()
 
-print(f"the current weather in {r_curr_dict['name']} is {r_curr_dict['main']['temp']}ºC")
+"""need to use datetime module to remove the time from dt_txt
+perhaps store date and temp as key value pair in a dict?"""
+for key in forecast_dict["list"]:
+    print(key["dt_txt"])
+    print(key["main"]["temp"])
+
+
+# print(f"the current weather in {curr_dict['name']} is {curr_dict['main']['temp']}ºC")
 
 
