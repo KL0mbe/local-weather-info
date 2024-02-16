5167e9131e0af46bc2e3534c26660798a47fbf8e
Metin Özkaya <klombe@outlook.dk>
2024-02-16 20:56:05 +0100

    Refactor forecast_temps to dict with weekdays as keys and temps as values, then update the values to avg temps.

--- weather_app.py
+++ weather_app.py
@@ -15,49 +15,16 @@ forecast_weather = requests.get(
 curr_dict = curr_weather.json()
 forecast_dict = forecast_weather.json()
 
-"""functioning temp separator by day; need to attempt it in a for loop to cut down on the if elif length"""
-forecast_temps = []
-forecast_days = []
+forecast_temps = {}
 for key in forecast_dict["list"]:
     dt = datetime.fromisoformat(key["dt_txt"])
     today = datetime.today().date()
-    if dt.date() == today + timedelta(days=1):
-        if len(forecast_temps) < 1:
-            forecast_temps.append([])
-        forecast_temps[0].append(key["main"]["temp"])
-        if len(forecast_days) < 1:
-            forecast_days.append(dt.strftime("%a"))
-    elif dt.date() == today + timedelta(days=2):
-        if len(forecast_temps) < 2:
-            forecast_temps.append([])
-        forecast_temps[1].append(key["main"]["temp"])
-        if len(forecast_days) < 2:
-            forecast_days.append(dt.strftime("%a"))
-    elif dt.date() == today + timedelta(days=3):
-        if len(forecast_temps) < 3:
-            forecast_temps.append([])
-        forecast_temps[2].append(key["main"]["temp"])
-        if len(forecast_days) < 3:
-            forecast_days.append(dt.strftime("%a"))
-    elif dt.date() == today + timedelta(days=4):
-        if len(forecast_temps) < 4:
-            forecast_temps.append([])
-        forecast_temps[3].append(key["main"]["temp"])
-        if len(forecast_days) < 4:
-            forecast_days.append(dt.strftime("%a"))
-    elif dt.date() == today + timedelta(days=5):
-        if len(forecast_temps) < 5:
-            forecast_temps.append([])
-        forecast_temps[4].append(key["main"]["temp"])
-        if len(forecast_days) < 5:
-            forecast_days.append(dt.strftime("%a"))
+    if dt.date() != today:
+        forecast_temps.setdefault(dt.strftime("%A"), []).append(key["main"]["temp"])
 
-forecast_sum = []
-for list in forecast_temps:
-    forecast_sum.append(sum(list)/len(list))
+for key in forecast_temps:
+    forecast_temps[key] = (sum(forecast_temps[key])/len(forecast_temps[key]))
 
-
-print(forecast_days)
-print(forecast_sum)
-print(f"the current weather in {curr_dict['name']} is {curr_dict['main']['temp']}ºC\n"
-      f"the average weather tomorrow will be {forecast_sum[0]}ºC")
+print(forecast_temps)
+# print(f"the current weather in {curr_dict['name']} is {curr_dict['main']['temp']}ºC\n"
+#       f"the average weather tomorrow will be {forecast_temps[0]}ºC")
