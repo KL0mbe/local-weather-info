229ef68e8cfab79eb0b55531a1046d1f89e50704
Metin Özkaya <klombe@outlook.dk>
2024-02-19 23:07:19 +0100

    removed redundant forecasts_list and add error handling
    replace forecast_list with the existing forecasts variable. add error handling for 4xx,5xx and wrong input errors, quit code on the former, repeat on the latter.

--- weather_app.py
+++ weather_app.py
@@ -1,11 +1,24 @@
 import requests
 import os
 from datetime import datetime
-city_name = input("please type your city name: ")
-country_code = input("please type your country code: ")
 api_key = os.environ.get("OPEN_WEATHER_API_KEY")
 
-location = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_code}&appid={api_key}")
+while True:
+    city_name = input("please type your city name: ")
+    country_code = input("please type your country code: ")
+
+    try:
+        location = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_code}&appid={api_key}")
+        location.raise_for_status()
+        if location.json():
+            break
+        else:
+            print("sorry there is no location with that name or country code\n")
+    except requests.RequestException as e:
+        print(f"error fetching data : {e}")
+        quit()
+
+
 lat, lon = location.json()[0]["lat"], location.json()[0]["lon"]
 curr_weather = requests.get(
     f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric")
@@ -25,11 +38,11 @@ for key in forecast_temps:
     forecast_temps[key] = (sum(forecast_temps[key])/len(forecast_temps[key]))
 
 day = 0
-forecasts_list = []
+forecasts = []
 for key in forecast_temps:
-    forecasts_list.append(f"{key} {round(forecast_temps[key], 2)}ºC")
+    forecasts.append(f"{key} {round(forecast_temps[key], 2)}ºC")
+forecasts = "\n".join(forecasts)
 
-forecasts = "\n".join(forecasts_list)
 print(f"the current weather in {curr_dict['name']} is {curr_dict['main']['temp']}ºC\n"
       f"the weather on the following days will be:\n"
       f"{forecasts}")
